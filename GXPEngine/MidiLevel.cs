954de4a36a7b8536879000f49ecbf175e03927eb
randarenko <minedanka@gmail.com>
2023-02-18 16:37:11 +0100

    gitigore corrected, started working on midiParser and all midi related stuff

---
+++ GXPEngine/MidiLevel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+	internal class MidiLevel
+	{
+		public List<MidiNote> Notes { get { return _notes; } }
+		/// <summary>
+		/// use this to offset ends of all notes on the level
+		/// </summary>
+		public float NoteEndOffset { get; set; }
+
+		private List<MidiNote> _notes= new List<MidiNote>();
+
+		public MidiLevel()
+		{
+
+		}
+	}
+}
---
+++ GXPEngine/MidiNote.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+	internal class MidiNote
+	{
+		/// <summary>
+		/// start of the note in ms since start of the track
+		/// </summary>
+		public long AbsoluteStart { get { return _absoluteStart; } }
+		/// <summary>
+		/// duration of the note
+		/// </summary>
+		public int Duration { get { return _duration; } }
+		/// <summary>
+		/// note pitch aka number, use that to differentiate between note lanes in the game
+		/// </summary>
+		public int Pitch { get { return _pitch; } }
+
+		private long _absoluteStart;
+		private int _duration;
+		private int _pitch;
+		public MidiNote(long absoluteStart, int duration, int pitch)
+		{
+			_absoluteStart = absoluteStart;
+			_duration = duration;
+			_pitch = pitch;
+
+		}
+	}
+}
